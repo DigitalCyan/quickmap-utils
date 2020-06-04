8e6f0aa4ae1c26a3759dc5763a65ae6eb876b621
DigitalCyan <marinoljubetic8@gmail.com>
2020-06-04 02:09:52 +0200

    Major UI updates and some functionality

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 const { ipcRenderer } = require("electron");
 
-document
-  .querySelector("#readBtn")
-  .addEventListener("click", () => ipcRenderer.send("read-config"));
+document.querySelector("#readBtn").addEventListener("click", () => {
+  updatePreview(ipcRenderer.sendSync("read-config"));
+});
+
+document.querySelector("#gitHubBtn").addEventListener("click", () => {
+  ipcRenderer.sendSync("open-github");
+})
+
+const updatePreview = (data) => {
+  document.querySelector("#previewParagraph").textContent = data;
+  conf = JSON.parse(data);
+};
+
+updatePreview(ipcRenderer.sendSync("read-config"));
--- src/main.js
+++ src/main.js
@@ -1,19 +1,36 @@
-const { app, BrowserWindow, ipcMain } = require('electron');
-const path = require('path');
+const { app, BrowserWindow, ipcMain } = require("electron");
+const { exec } = require("child_process");
+const path = require("path");
+const fs = require("fs");
 
-app.on('ready', () => {
+app.on("ready", () => {
   const mainWin = new BrowserWindow({
-    width: 600,
-    height: 500,
+    width: 800,
+    height: 600,
     webPreferences: {
-      nodeIntegration: true
-    }
+      nodeIntegration: true,
+    },
   });
 
   mainWin.menuBarVisible = false;
-  mainWin.loadFile('./src/index.html')
-})
+  mainWin.loadFile("./src/index.html");
+});
 
-ipcMain.on('read-config', (e) => {
-  console.log('reading config.json...')
-})
+ipcMain.on("read-config", (e) => {
+  e.returnValue = fs
+    .readFileSync(path.join(__dirname, "../config.json"))
+    .toString();
+});
+
+ipcMain.on("write-config", (e) => {
+  console.log(
+    fs.readFileSync(path.join(__dirname, "../config.json")).toString()
+  );
+  e.returnValue = fs
+    .readFileSync(path.join(__dirname, "../config.json"))
+    .toString();
+});
+
+ipcMain.on("open-github", () => {
+  exec("start https://www.github.com/DigitalCyan");
+});
